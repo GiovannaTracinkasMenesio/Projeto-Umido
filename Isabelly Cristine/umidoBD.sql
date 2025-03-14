5e6e982695c95f1c911260eaa437ec3baf5175ec
Isabelly Cristine <isabelly.godoi@sptech.school>
2025-03-13 22:50:59 -0300

    Add files via upload

---
+++ Isabelly Cristine/umidoBD.sql
@@ -0,0 +1,49 @@
+create database umido;
+use umido;
+
+create table plano(
+	id_plano int primary key auto_increment,
+	tipo_plano varchar(20),
+    valor decimal(4,2)
+);
+
+create table tipo_plantacao(
+	id_tipo_plantacao int primary key auto_increment,
+	tipo_plantacao varchar(20)
+);
+
+create table endereco(
+	id_endereco int primary key auto_increment,
+    id_usuario int not null,
+    uf varchar(2) not null,
+    cep char(9) not null,
+    logradouro varchar(70),
+    nome_logradouro varchar(70),
+    numero_logradouro varchar(70),
+    bairro varchar(50)
+);
+
+create table usuario(
+	id_usuario int primary key auto_increment,
+    id_endereco int,
+    id_plano int,
+    id_tipo_plantacao int,
+    cpf char(11) not null,
+    nome varchar(50) not null,
+    email varchar(70) not null,
+    telefone varchar(14) not null,
+    dt_nascimento date not null,
+    foreign key (id_endereco) references endereco(id_endereco),
+    foreign key (id_plano) references plano(id_plano),
+    foreign key (id_tipo_plantacao) references tipo_plantacao(id_tipo_plantacao)
+);
+
+create table arduino (
+	id_arduino int primary key auto_increment,
+    nome_arduino varchar(50),
+    umidade float,
+    temperatura float,
+    minimo_umidade float,
+    máxima_temperatura float
+);
+
